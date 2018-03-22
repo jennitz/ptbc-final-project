84021da939b9e2762468648a9a721ddcce96b11b
Elisia Morgan <elisiamorgan@quickenloans.com>
2018-03-22 18:45:09 -0400

    carousel feature

--- public/home/home-controller.js
+++ public/home/home-controller.js
@@ -3,9 +3,9 @@
     .controller('HomeController', function(){
 		var vm = this;
 		vm.slides = [
-			{image: 'https://www.nestle.com.sg/asset-library/PublishingImages/Media/NHW/nutrition_for_family/img_workplace_workout.jpg'},
-			{image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAGSX5-_d0Oem2sXcH-nP7SVvrIM2RjDkLO4gMZEL-PRFREnfX7g'},
-			{image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRshwhbVa68m4hxEBN-nI4OmA9CAoBqb4icobLKmINFY6aUEbTPlA'},
+			{src: 'https://www.nestle.com.sg/asset-library/PublishingImages/Media/NHW/nutrition_for_family/img_workplace_workout.jpg'},
+			{src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAGSX5-_d0Oem2sXcH-nP7SVvrIM2RjDkLO4gMZEL-PRFREnfX7g'},
+			{src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRshwhbVa68m4hxEBN-nI4OmA9CAoBqb4icobLKmINFY6aUEbTPlA'},
 		
 
 		];
