de77cc1255e0df215f10b47a404d4947eb3a80f5
Elisia Morgan <elisiamorgan@quickenloans.com>
2018-03-26 16:27:18 -0400

    pictures uploaded

--- public/home/home-controller.js
+++ public/home/home-controller.js
@@ -3,9 +3,9 @@
     .controller('HomeController', function(){
 		var vm = this;
 		vm.slides = [
-			{src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAiykJO0XypIFDWwVvMlODraznt-h8xi04-tMqQJ_GL0yVrVoTdw'},
-			{src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAGSX5-_d0Oem2sXcH-nP7SVvrIM2RjDkLO4gMZEL-PRFREnfX7g'},
-			{src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRshwhbVa68m4hxEBN-nI4OmA9CAoBqb4icobLKmINFY6aUEbTPlA'},
+			{src: 'images/fit3.jpg'},
+			{src: 'images/fit4.jpg'},
+			{src: 'images/fit6.jpg'},
 		
 
 		];
